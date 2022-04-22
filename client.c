9a8d83f4b508c899f6d83558a7f85fe3c61974cd
zu-23 <h.zakaria52@gmail.com>
2022-04-22 18:14:06 +0000

    unfinished

--- client.c
+++ client.c
@@ -3,7 +3,26 @@
 #include<signal.h>
 #include<sys/wait.h>
 #include "LIBFT_42/libft.h"
+int	checkpid(char *pid, int arg)
+{
+	int i;
 
+	i = -1;
+	if (arg != 3)
+	{
+		printf("wrong number of arguments\n");
+		return (1);
+	}
+	while (pid[++i])
+	{
+		if (ft_isalpha(pid[i]) == 1)
+			{
+				printf("wrong pid!\n");
+				return (1);
+			}
+	}
+	return (0);
+}
 	
 int	main(int argc, char **argv)
 {
@@ -12,37 +31,32 @@ int	main(int argc, char **argv)
    int		bit;
    char 	*msg;
 	
+	if (checkpid(argv[1],argc) == 1)
+		return (0);
 	pid = atoi(argv[1]);
   	bit = 0;
 	i = 0;
 	msg = argv[2];
-//    if (argc != 2)
-//    	return (1);
   	while(msg[i])
 	{
 		while (bit < 8)
 		{
 			if (msg[i] & 128 >> bit)
-			{
 				kill(pid, SIGUSR1);
-				usleep(100);
-			}
 			else
-			{
 				kill(pid,SIGUSR2);
-				usleep(100);
-			}
+
+			usleep(100);
 			bit++;
 		}
 		bit = 0;
 		i++;
 	}
-	usleep(100);
-	while (bit < 8)
-	{
-		//printf("%d	",bit);
-		kill(pid,SIGUSR2);
-		bit++;
-		usleep(40);
-	}
+	// while (bit < 8)
+	// {
+	// 	//printf("%d	",bit);
+	// 	kill(pid,SIGUSR2);
+	// 	bit++;
+	// 	usleep(40);
+	// }
 }
--- server.c
+++ server.c
@@ -4,58 +4,54 @@
 #include<sys/wait.h>
 #include<stdlib.h>
 #include"LIBFT_42/libft.h"
-int strl(char *str)
-{
-	int len;
+// int strl(char *str)
+// {
+// 	int len;
 
-	len = 0;
-	if (!str)
-		return(0);
-	while (str[len])
-		len++;
+// 	len = 0;
+// 	if (!str)
+// 		return(0);
+// 	while (str[len])
+// 		len++;
 
-	return(len);
-}
-char *msgconc(char **msg,char letter)
-{
-	int 	len;
-	char	*tmp;
-	int		i;
+// 	return(len);
+// }
+// char *msgconc(char **msg,char letter)
+// {
+// 	int 	len;
+// 	char	*tmp;
+// 	int		i;
 
-	if (!letter)
-		return (NULL);
-	len = strl((*msg));
-	tmp = (char *)malloc(len + 2);
-	i = -1;
-	if ((*msg))
-	{
-		while ((*msg)[++i])
-			tmp[i] = (*msg)[i];
-	free((*msg));
-	}
-	if (i == -1)
-		tmp[++i] = letter;
-	else
-		tmp[i] = letter;
-	tmp[++i] = '\0';
-	*msg = tmp;
-}
+// 	if (!letter)
+// 		return (NULL);
+// 	len = strl((*msg));
+// 	tmp = (char *)malloc(len + 2);
+// 	i = -1;
+// 	if ((*msg))
+// 	{
+// 		while ((*msg)[++i])
+// 			tmp[i] = (*msg)[i];
+// 	free((*msg));
+// 	}
+// 	if (i == -1)
+// 		tmp[++i] = letter;
+// 	else
+// 		tmp[i] = letter;
+// 	tmp[++i] = '\0';
+// 	*msg = tmp;
+// }
 
 void handle_sigint(int sig)
 {
    static char	letter;
-   char 		*msg;
    static int	bin;
    static int	bits;
-   static int	null;
 
-	msg = NULL;
 	if (bits > 7 || bin == 0)
 	{
 		bits = 0;
 		letter = 0;
 		bin = 128;
-		null = 0;
 	}
 	if (sig == SIGUSR1)
 	{
@@ -67,23 +63,13 @@ void handle_sigint(int sig)
 	{
 		bin /= 2;
 		bits++;
-		//printf("null:%d bits:%d\n",null,bits);
-		null++;
 	}
 	if( bits > 7)
 	{
-		//printf("%d\n",null);
-		msgconc(&msg, letter);
-		printf("null: %d\n and msg: %s\n",null,msg);
-		if (null > 7)
-		{
-			printf("whats my value %d\n",null);
-			printf("%s\n",msg);
-			//free(msg);
-			//msg = NULL;
-		}
+		printf("	%d\n",letter);
+		write(1, &letter, 1);
 	}
-	
+
 }
   
 int main()
